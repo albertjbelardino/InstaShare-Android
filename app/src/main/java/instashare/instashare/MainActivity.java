6b2d426083748dab649e58409ec2dd912f2e8413
albie <albertjbelardino@gmail.com>
2019-02-19 02:07:29 -0500

    first commit

---
+++ app/src/main/java/instashare/instashare/MainActivity.java
@@ -0,0 +1,13 @@
+package instashare.instashare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
