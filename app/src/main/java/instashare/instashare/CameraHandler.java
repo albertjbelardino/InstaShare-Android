9969c543125ade16898d5114cb53eee2d757709e
Jakk-Git <JacksonPotash@gmail.com>
2019-02-25 23:46:16 -0500

    Can take and save images now. They always return in landscape and it is a pain to make them not save in landscape apparently, so hopefully that's fine.

--- app/src/main/java/instashare/instashare/CameraHandler.java
+++ app/src/main/java/instashare/instashare/CameraHandler.java
@@ -2,9 +2,14 @@ package instashare.instashare;
 
 import android.Manifest;
 import android.app.Activity;
+import android.content.ContentValues;
 import android.content.Context;
+import android.content.ContextWrapper;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Camera;
+import android.graphics.ImageFormat;
 import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraCaptureSession;
@@ -12,14 +17,28 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.hardware.camera2.CaptureRequest;
+import android.hardware.camera2.TotalCaptureResult;
 import android.hardware.camera2.params.StreamConfigurationMap;
+import android.media.Image;
+import android.media.ImageReader;
+import android.os.Environment;
 import android.os.Handler;
+import android.os.Parcel;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.Surface;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -32,6 +51,10 @@ public class CameraHandler {
     final CameraManager cm;
     Activity a;
     CameraDevice cd;
+    Boolean takePicture = false;
+    ImageReader ir;
+    Surface irsurface;
+
 
 
 
@@ -39,12 +62,20 @@ public class CameraHandler {
     public CameraHandler(SurfaceView sv, Activity a) throws CameraAccessException {
         this.a = a;
         this.sv = sv;
+
+        final Activity getActivity = a;
         cm = (CameraManager) a.getSystemService(Context.CAMERA_SERVICE);
+        ir = ImageReader.newInstance(cm.getCameraCharacteristics(cm.getCameraIdList()[1])
+                        .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                        .getOutputSizes(SurfaceHolder.class)[0].getWidth(),
+                cm.getCameraCharacteristics(cm.getCameraIdList()[0])
+                        .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
+                        .getOutputSizes(SurfaceHolder.class)[0].getHeight(), ImageFormat.JPEG, 1);
         final SurfaceView finalview = sv;
          SurfaceHolder surfhold = finalview.getHolder();
         surfhold.setKeepScreenOn(true);
         try {
-            surfhold.setFixedSize(cm.getCameraCharacteristics(cm.getCameraIdList()[0])
+            surfhold.setFixedSize(cm.getCameraCharacteristics(cm.getCameraIdList()[1])
                             .get(CameraCharacteristics.SCALER_STREAM_CONFIGURATION_MAP)
                             .getOutputSizes(SurfaceHolder.class)[0].getWidth(),
                     cm.getCameraCharacteristics(cm.getCameraIdList()[0])
@@ -55,15 +86,54 @@ public class CameraHandler {
             e.printStackTrace();
         }
         final Surface surface = surfhold.getSurface();
-        final List<Surface> list = Arrays.asList(surface);
+        final List<Surface> list = new ArrayList<Surface>();
+        ir.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener() {
+            @Override
+            public void onImageAvailable(ImageReader imageReader) {
+                Image image = imageReader.acquireLatestImage();
+                Image.Plane[] planes = image.getPlanes();
+                ByteBuffer buffer = planes[0].getBuffer();
+                buffer.rewind();
+                byte[] data = new byte[buffer.capacity()];
+                buffer.get(data);
+                Bitmap mybitmap = BitmapFactory.decodeByteArray(data, 0, data.length);
+                Log.d("ADASD", mybitmap.toString());
+                String filename = System.currentTimeMillis() + ".jpg";
+                File file = new File(Environment.getExternalStorageDirectory()+"/Instashare");
+                if(!file.exists()){
+                    file.mkdir();
+                }
+
+                try{
+                    File finalfile = new File(file, filename);
+                    FileOutputStream fos = new FileOutputStream(finalfile);
+                    mybitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                    fos.flush();
+                    fos.close();
+
+                }catch (Exception e){
+                    e.printStackTrace();
+
+                }
+                image.close();
+
+
+
+            }
+        }, sv.getHandler());
+        irsurface = ir.getSurface();
+        list.add(surface);
+        list.add(irsurface);
+
         doMoreCameraStuff(list);
     }
 
 
     public void doMoreCameraStuff(List<Surface> ls) throws CameraAccessException {
         while (true) {
-            if (ActivityCompat.checkSelfPermission(a, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(a, new String[]{Manifest.permission.CAMERA}, 0);
+            if (ActivityCompat.checkSelfPermission(a, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
+                    ActivityCompat.checkSelfPermission(a, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(a, new String[]{Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE}, 0);
                 //do something
                 // TODO: Consider calling
                 //    ActivityCompat#requestPermissions
@@ -97,8 +167,10 @@ public class CameraHandler {
                                         ccsession.setRepeatingRequest(cr.build(), new CameraCaptureSession.CaptureCallback() {
                                             @Override
                                             public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+
                                                 super.onCaptureStarted(session, request, timestamp, frameNumber);
                                             }
+
                                         }, sv.getHandler());
                                     } catch (CameraAccessException e) {
                                         e.printStackTrace();
@@ -145,6 +217,27 @@ public class CameraHandler {
         }, sv.getHandler());
 
 
+    }
+
+    public void endCapture()
+    {
+        ccsession.close();
+        cd.close();
+        sv.getHolder().getSurface().release();
+        ir.close();
+
+    }
+
+
+    public void takePictureNow() throws CameraAccessException {
+        CaptureRequest.Builder cr = cd.createCaptureRequest(CameraDevice.TEMPLATE_PREVIEW);
+        cr.addTarget(irsurface);
+        ccsession.capture(cr.build(), new CameraCaptureSession.CaptureCallback() {
+            @Override
+            public void onCaptureStarted(@NonNull CameraCaptureSession session, @NonNull CaptureRequest request, long timestamp, long frameNumber) {
+                super.onCaptureStarted(session, request, timestamp, frameNumber);
+            }
+        }, sv.getHandler());
     }
 
 }
--- app/src/main/java/instashare/instashare/MainActivity.java
+++ app/src/main/java/instashare/instashare/MainActivity.java
@@ -6,20 +6,53 @@ import android.hardware.camera2.CameraAccessException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button takepicbutton;
+    SurfaceView sv;
+    CameraHandler ch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setUpPictureTaking();
+
+    }
+
+    public void setUpPictureTaking()
+    {
+        takepicbutton = findViewById(R.id.pictureButton);
+        sv = (SurfaceView)findViewById(R.id.backgroundCamera);
+
         try {
-            SurfaceView sv = (SurfaceView)findViewById(R.id.backgroundCamera);
+
             sv.setVisibility(SurfaceView.VISIBLE);
             sv.draw(new Canvas());
-            CameraHandler ch = new CameraHandler(sv, (Activity)this);
+            ch = new CameraHandler(sv, (Activity)this);
+            takepicbutton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    try {
+                        ch.takePictureNow();
+                    } catch (CameraAccessException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
         } catch (CameraAccessException e) {
             e.printStackTrace();
         }
+
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        ch.endCapture();
+        super.onDestroy();
     }
 }
