28714eb1ba5c1cec7837dea01814234cd02be6b6
Jakk-Git <JacksonPotash@gmail.com>
2019-03-11 13:08:35 -0400

    Added contact list. Tried to add correct photo orientation but it's looking to be very difficult to impliment.

---
+++ app/src/main/java/instashare/instashare/SignUpLogInActivity.java
@@ -0,0 +1,13 @@
+package instashare.instashare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUpLogInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_log_in);
+    }
+}
