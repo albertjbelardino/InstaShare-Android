7871d112253f628fd18d0c8e4b2eacc85010de3b
Albert <albert.belardino@cylitix.com>
2019-04-07 20:05:31 -0400

    single picture gallery choice is now synced with choose send activity

--- app/src/main/java/instashare/instashare/VolleyFactory.java
+++ app/src/main/java/instashare/instashare/VolleyFactory.java
@@ -1,6 +1,7 @@
 package instashare.instashare;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 
 import com.android.volley.AuthFailureError;
@@ -9,13 +10,18 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static android.support.v4.content.ContextCompat.startActivity;
 
 public class VolleyFactory {
 
@@ -49,6 +55,60 @@ public class VolleyFactory {
         jor.setRetryPolicy(new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
         rq.add(jor);
         return responseHolder[0];
+    }
+
+    public static void sendJsonArrayRequestWithJsonObject(JSONObject jsonob,
+                                                          Context applicationContext, String apiUrl,
+                                                          final Context callingContext, final String imagepath) {
+
+        final JSONArray[] responseHolder = new JSONArray[1];
+        final Intent[] intentHolder = new Intent[1];
+        responseHolder[0] = new JSONArray();
+        RequestQueue rq = Volley.newRequestQueue(applicationContext);
+        JsonArrayRequest jor = new JsonArrayRequest(Request.Method.POST, ApiContract.sendPicture(), jsonob, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                if(response.length() == 0)
+                {
+                    Log.d("response_len", "response len is 0");
+                }
+                else {
+                    Log.d("response", response.toString());
+                    String[] numbers = new String[response.length()];
+                    String[] names = new String[response.length()];
+                    for (int x = 0; x < response.length(); x++) {
+                        try {
+                            numbers[x] = response.getJSONObject(x).getString("phone_number");
+                            names[x] = response.getJSONObject(x).getString("name");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    Intent i = new Intent(callingContext, ChooseSendActivity.class);
+                    i.putExtra("contact_names", names);
+                    i.putExtra("contact_numbers", numbers);
+                    i.putExtra("myimagepath", imagepath);
+                    callingContext.startActivity(i);
+                }
+            }
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Error", error.toString());
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+
+                Map<String, String> headers = new HashMap<>();
+                headers.put("Authorization", "Bearer " +
+                        LoginService.jwt_token);
+                return headers;
+            }
+        };
+        jor.setRetryPolicy(new DefaultRetryPolicy(30000, DefaultRetryPolicy.DEFAULT_MAX_RETRIES, DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+        rq.add(jor);
     }
 }
 
