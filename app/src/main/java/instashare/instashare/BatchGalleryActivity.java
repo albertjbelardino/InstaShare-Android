bd119da4db11d25a889a22a3f9f417b451a568e0
albertjbelardino <albertjbelardino@gmail.com>
2019-04-14 13:03:17 +0900

    have a remove image button, don't think it is working properly right now though

--- app/src/main/java/instashare/instashare/BatchGalleryActivity.java
+++ app/src/main/java/instashare/instashare/BatchGalleryActivity.java
@@ -1,10 +1,16 @@
 package instashare.instashare;
 
 import android.content.ClipData;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -13,6 +19,7 @@ public class BatchGalleryActivity extends AppCompatActivity {
 
     ViewPager galleryViewPager;
     GalleryCardAdapter galleryCardAdapter;
+    Button removeButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +31,13 @@ public class BatchGalleryActivity extends AppCompatActivity {
         galleryCardAdapter = new GalleryCardAdapter(this, imagePaths);
         galleryViewPager = (ViewPager) findViewById(R.id.batchGalleryViewPager);
         galleryViewPager.setAdapter(galleryCardAdapter);
+
+        removeButton = (Button) findViewById(R.id.removeGalleryPictureButton);
+        removeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                galleryViewPager.removeView(galleryViewPager.getChildAt(galleryViewPager.getCurrentItem()));
+            }
+        });
     }
 }
