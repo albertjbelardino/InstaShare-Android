95700eed696250880726cbbd8ece12ae9ee6a452
Scott Glascott <Scott.Glascott@gmail.com>
2019-04-15 16:47:04 -0400

    added batch upload url

--- app/src/main/java/instashare/instashare/ApiContract.java
+++ app/src/main/java/instashare/instashare/ApiContract.java
@@ -17,4 +17,6 @@ public class ApiContract {
     }
 
     public static String sendPicture(){return baseUrl + "api/singlephotoMobile/";}
+
+    public static String batchUpload(){return baseUrl + "api/batchuploadMobile/";}
 }
