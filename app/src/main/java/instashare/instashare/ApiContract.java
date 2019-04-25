c2358de5db6e1e74ec958667b26c8b02c17eb6d9
albie <albertjbelardino@gmail.com>
2019-04-25 01:12:37 -0400

    added twilio to project

--- app/src/main/java/instashare/instashare/ApiContract.java
+++ app/src/main/java/instashare/instashare/ApiContract.java
@@ -1,8 +1,13 @@
 package instashare.instashare;
 
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
 public class ApiContract {
 
     public static final String baseUrl = "http://django-env.mzkdgeh5tz.us-east-1.elasticbeanstalk.com:80/";
+    public static final String twilioSID = "SK3e5cd4ee9d4317bb2aae7da8e01f0075";
 
     public static String loginUrl() {
         return baseUrl + "api/token/";
