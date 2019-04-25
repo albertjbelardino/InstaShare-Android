76e302fcf6e973bef52a77730980c9364c038896
albie <albertjbelardino@gmail.com>
2019-04-25 03:44:03 -0400

    test method created in ApiContract Class, need to handle new batch response in volley factory and adjust sending logic in BatchSendActivity

--- app/src/main/java/instashare/instashare/ApiContract.java
+++ app/src/main/java/instashare/instashare/ApiContract.java
@@ -4,14 +4,27 @@ import com.twilio.Twilio;
 import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
+import java.net.URI;
+import java.util.List;
+
 public class ApiContract {
 
     public static final String baseUrl = "http://django-env.mzkdgeh5tz.us-east-1.elasticbeanstalk.com:80/";
+    public static final String twilioInstasharePhoneNumber = "+12672140841";
     public static final String twilioSID  = "";
     public static final String twilioAuth = "";
 
     public static String loginUrl() {
         return baseUrl + "api/token/";
+    }
+
+    public static void sendTwilioMessage(String recipientPhoneNumber, List<URI> imageUrls) {
+        Twilio.init("", "");
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber(twilioInstasharePhoneNumber),
+                new com.twilio.type.PhoneNumber("+1" + recipientPhoneNumber),
+                imageUrls
+        ).create();
     }
 
     public static String contactUploadUrl() {
